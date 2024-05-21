b29c11fd665ce6878bf6734697dbdb9a86149082
Thlamz <thlamenza@hotmail.com>
2024-05-20 22:49:11 -0300

    Setting up ablation test campaign

--- scenario/002-scaling/benchmark.py
+++ scenario/002-scaling/benchmark.py
@@ -21,16 +21,14 @@ class Args:
 
 # Write the commands above in array form
 experiments = [
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=1", "--exp_name=1-sensor-soft", "--no-checkpoint-visual-evaluation", "--run-name=SingleDrone", "--checkpoint-freq=1000000", "--total-timesteps=10000000"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=2", "--exp_name=2-sensor-soft", "--no-checkpoint-visual-evaluation", "--run-name=SingleDrone", "--checkpoint-freq=1000000", "--total-timesteps=10000000"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=3", "--exp_name=3-sensor-soft", "--no-checkpoint-visual-evaluation", "--run-name=SingleDrone", "--checkpoint-freq=1000000", "--total-timesteps=10000000"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=4", "--exp_name=4-sensor-soft", "--no-checkpoint-visual-evaluation", "--run-name=SingleDrone", "--checkpoint-freq=1000000", "--total-timesteps=10000000"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=5", "--exp_name=5-sensor-soft", "--no-checkpoint-visual-evaluation", "--run-name=SingleDrone", "--checkpoint-freq=1000000", "--total-timesteps=10000000"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=6", "--exp_name=6-sensor-soft", "--no-checkpoint-visual-evaluation", "--run-name=SingleDrone", "--checkpoint-freq=1000000", "--total-timesteps=10000000"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=7", "--exp_name=7-sensor-soft", "--no-checkpoint-visual-evaluation", "--run-name=SingleDrone", "--checkpoint-freq=1000000", "--total-timesteps=10000000"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=8", "--exp_name=8-sensor-soft", "--no-checkpoint-visual-evaluation", "--run-name=SingleDrone", "--checkpoint-freq=1000000", "--total-timesteps=10000000"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=9", "--exp_name=9-sensor-soft", "--no-checkpoint-visual-evaluation", "--run-name=SingleDrone", "--checkpoint-freq=1000000", "--total-timesteps=10000000"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=10", "--exp_name=10-sensor-soft", "--no-checkpoint-visual-evaluation", "--run-name=SingleDrone", "--checkpoint-freq=1000000", "--total-timesteps=10000000"],
+    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=6", "--exp_name=6-sensor-soft", "--no-checkpoint-visual-evaluation", "--run-name=StallFix", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--max_seconds_stalled=60"],
+    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=6", "--exp_name=control", "--no-checkpoint-visual-evaluation", "--run-name=Ablation", "--checkpoint-freq=1000000", "--total-timesteps=10000000"],
+    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=6", "--exp_name=hard-reward", "--no-checkpoint-visual-evaluation", "--run-name=Ablation", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--no-soft-reward"],
+    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=6", "--exp_name=big-state", "--no-checkpoint-visual-evaluation", "--run-name=Ablation", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--no-state_relative_positions"],
+    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=6", "--exp_name=train_once", "--no-checkpoint-visual-evaluation", "--run-name=Ablation", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--no-train_once_for_each_agent"],
+    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=6", "--exp_name=punish-out-of-bounds", "--no-checkpoint-visual-evaluation", "--run-name=Ablation", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--no-block_out_of_bounds"],
+    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=6", "--exp_name=max-simulation_length", "--no-checkpoint-visual-evaluation", "--run-name=Ablation", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--max_episode_length=200", "--max_seconds_stalled=500"],
+
 ]
 
 def run_experiment(experiment):
