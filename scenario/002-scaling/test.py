315c6242f8b623c6a9d981429bfed3c4da2f60b0
Thlamz <thlamenza@hotmail.com>
2024-05-25 11:28:37 -0300

    Fixing testing script

--- scenario/002-scaling/test.py
+++ scenario/002-scaling/test.py
@@ -3,18 +3,20 @@ from environment import GrADySEnvironment
 import torch
 
 # Loading the model
-model_path = f"runs/SingleDrone__2-sensor-soft__1__1715958003/2-sensor-soft-checkpoint500.cleanrl_model"
+model_path = f"runs/sensors/3-sensor-soft/3-sensor-soft-checkpoint223.cleanrl_model"
+print(f"Loading model from {model_path}")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+actor_model = torch.load(model_path, map_location=device)[0]
 
 if __name__ == "__main__":
-    print(f"Loading model from {model_path}")
-    actor_model = torch.load(model_path)[0]
-    for _ in range(10):
+    for i in range(10):
+        print(f"Running experiment: repetition {i}")
         # Creating environment
         env = GrADySEnvironment(
             algorithm_iteration_interval=0.5,
             render_mode="visual",
             num_drones=1,
-            num_sensors=2,
+            num_sensors=3,
             max_seconds_stalled=30,
             scenario_size=100,
             randomize_sensor_positions=True,
@@ -23,16 +25,12 @@ if __name__ == "__main__":
             state_num_closest_drones=2,
         )
 
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-
         actor = Actor(env.action_space(0), env.observation_space(0)).to(device)
         actor.load_state_dict(actor_model)
 
         # Running the model
         obs, _ = env.reset()
-        for _ in range(100):
+        while True:
             with torch.no_grad():
                 actions = {}
                 for agent in env.agents:
@@ -41,5 +39,6 @@ if __name__ == "__main__":
             next_obs, rewards, terminations, truncations, infos = env.step(actions)
             obs = next_obs
             if len(infos) > 0 and "avg_reward" in infos[env.agents[0]]:
+                print(f"Experiment finished. Success: {infos[env.agents[0]]['success']}")
                 break
         env.close()
