d3177581a5392746540a00a4822e262928da13ab
Thlamz <thlamenza@hotmail.com>
2024-05-27 09:56:00 -0300

    Setting up num closest sensors campaign

--- scenario/002-scaling/benchmark.py
+++ scenario/002-scaling/benchmark.py
@@ -21,11 +21,10 @@ class Args:
 
 # Write the commands above in array form
 experiments = [
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=4", "--exp_name=all_positions", "--no-checkpoint-visual-evaluation", "--run-name=StateModes", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--state-mode=all_positions"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=4", "--exp_name=absolute", "--no-checkpoint-visual-evaluation", "--run-name=StateModes", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--state-mode=absolute"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=4", "--exp_name=relative", "--no-checkpoint-visual-evaluation", "--run-name=StateModes", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--state-mode=relative"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=4", "--exp_name=distance_angle", "--no-checkpoint-visual-evaluation", "--run-name=StateModes", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--state-mode=distance_angle"],
-    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=4", "--exp_name=angle", "--no-checkpoint-visual-evaluation", "--run-name=StateModes", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--state-mode=angle"],
+    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=4", "--exp_name=1-closest", "--no-checkpoint-visual-evaluation", "--run-name=ClosestSensors", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--state-num-closest-sensors=1"],
+    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=4", "--exp_name=2-closest", "--no-checkpoint-visual-evaluation", "--run-name=ClosestSensors", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--state-num-closest-sensors=2"],
+    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=4", "--exp_name=3-closest", "--no-checkpoint-visual-evaluation", "--run-name=ClosestSensors", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--state-num-closest-sensors=3"],
+    ["python", "main.py", "--scenario_size=100", "--num-drones=1", "--num-sensors=4", "--exp_name=4-closest", "--no-checkpoint-visual-evaluation", "--run-name=ClosestSensors", "--checkpoint-freq=1000000", "--total-timesteps=10000000", "--state-num-closest-sensors=4"],
 ]
 
 def run_experiment(experiment):
