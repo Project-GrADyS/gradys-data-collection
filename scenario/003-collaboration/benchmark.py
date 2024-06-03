97166dd81197a740068aa34db1033a39b6364aee
Thlamz <thlamenza@hotmail.com>
2024-06-02 22:44:06 -0300

    Setting up simulation to test new scenario

--- scenario/003-collaboration/benchmark.py
+++ scenario/003-collaboration/benchmark.py
@@ -10,23 +10,17 @@ class Args:
     concurrency: int = 3
 
 # Commands:
-# python main.py --scenario_size=50 --num-drones=1 --num-sensors=1 --exp_name=1-sensor-1-drone-50-size --max_episode_length=60
-# python main.py --scenario_size=50 --num-drones=1 --num-sensors=2 --exp_name=2-sensor-1-drone-50-size --max_episode_length=60
-# python main.py --scenario_size=50 --num-drones=2 --num-sensors=1 --exp_name=1-sensor-2-drone-50-size --max_episode_length=60
-# python main.py --scenario_size=50 --num-drones=2 --num-sensors=2 --exp_name=2-sensor-2-drone-50-size --max_episode_length=60
-# python main.py --scenario_size=100 --num-drones=1 --num-sensors=1 --exp_name=1-sensor-1-drone-100-size --max_episode_length=60
-# python main.py --scenario_size=100 --num-drones=1 --num-sensors=2 --exp_name=2-sensor-1-drone-100-size --max_episode_length=60
-# python main.py --scenario_size=100 --num-drones=2 --num-sensors=1 --exp_name=1-sensor-2-drone-100-size --max_episode_length=60
-# python main.py --scenario_size=100 --num-drones=2 --num-sensors=2 --exp_name=2-sensor-2-drone-100-size --max_episode_length=60
+# python main.py --num-drones=1 --num-sensors=1 --state_num_closest_sensors=1 --state-num-closest-drones=0 --min-sensor-priority=1
 
 # Write the commands above in array form
 experiments = [
-
+    ["python", "main.py", "--num-drones=1", "--num-sensors=2", "--state_num_closest_sensors=1", "--state-num-closest-drones=0", "--min-sensor-priority=1", "--exp-name", "1-drone-closest-1-sensor-0-drone"],
+    ["python", "main.py", "--num-drones=1", "--num-sensors=2", "--state_num_closest_sensors=2", "--state-num-closest-drones=0", "--min-sensor-priority=1", "--exp-name", "1-drone-closest-2-sensor-0-drone"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--state_num_closest_sensors=1", "--state-num-closest-drones=0", "--min-sensor-priority=1", "--exp-name", "2-drone-closest-1-sensor-0-drone"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--state_num_closest_sensors=2", "--state-num-closest-drones=0", "--min-sensor-priority=1", "--exp-name", "2-drone-closest-2-sensor-0-drone"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--state_num_closest_sensors=1", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--exp-name", "2-drone-closest-1-sensor-1-drone"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--exp-name", "2-drone-closest-2-sensor-1-drone"],
 ]
-
-for s in [1, 2, 4]:
-    for a in [0, 1, 3]:
-        experiments.append(["python", "main.py", "--scenario_size=100", "--num-drones=4", "--num-sensors=8", f"--exp_name={s}-sensor-{a}-agent", "--no-checkpoint-visual-evaluation", "--run-name=Closest", "--checkpoint-freq=1000000", "--total-timesteps=10000000", f"--state-num-closest-sensors={s}", f"--state-num-closest-drones={a}"])
 
 def run_experiment(experiment):
     print("Running experiment: ", experiment)
