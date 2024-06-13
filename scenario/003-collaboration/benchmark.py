c394b70a00e1ec710def69be04185fc9214c85d8
Thlamz <thlamenza@hotmail.com>
2024-06-12 23:09:24 -0300

    Setting up hyperparameter search

--- scenario/003-collaboration/benchmark.py
+++ scenario/003-collaboration/benchmark.py
@@ -10,13 +10,22 @@ class Args:
     concurrency: int = 3
 
 # Commands:
-# python main.py --num-drones=1 --num-sensors=1 --state_num_closest_sensors=1 --state-num-closest-drones=0 --min-sensor-priority=1
+# python main.py --num-drones=2 --num-sensors=2 --run-name=centralized-critic --exp-name=yes --state_num_closest_sensors=2 --state-num-closest-drones=1 --min-sensor-priority=1 --centralized_critic --total-timesteps=10000000
 
 # Write the commands above in array form
 experiments = [
-    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--run-name=centralized-critic", "--exp-name=yes",  "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--centralized_critic", "--total-timesteps=10000000"],
-    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--run-name=centralized-critic", "--exp-name=no",  "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--no-centralized_critic", "--total-timesteps=10000000"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--run-name=tau", "--exp-name=0.005", "--tau=0.005",  "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--centralized_critic", "--total-timesteps=10000000"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--run-name=tau", "--exp-name=0.05", "--tau=0.05",  "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--centralized_critic", "--total-timesteps=10000000"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--run-name=tau", "--exp-name=0.5", "--tau=0.05",  "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--centralized_critic", "--total-timesteps=10000000"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--run-name=policy-frequency", "--exp-name=1", "--policy-frequency=1",  "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--centralized_critic", "--total-timesteps=10000000"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--run-name=policy-frequency", "--exp-name=2", "--policy-frequency=2",  "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--centralized_critic", "--total-timesteps=10000000"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--run-name=policy-frequency", "--exp-name=2", "--policy-frequency=3",  "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--centralized_critic", "--total-timesteps=10000000"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--run-name=lr", "--exp-name=3e-3", "--learning_rate=3e-3",  "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--centralized_critic", "--total-timesteps=10000000"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--run-name=lr", "--exp-name=3e-4", "--learning_rate=3e-4",  "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--centralized_critic", "--total-timesteps=10000000"],
+    ["python", "main.py", "--num-drones=2", "--num-sensors=2", "--run-name=lr", "--exp-name=3e-5", "--learning_rate=3e-5",  "--state_num_closest_sensors=2", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--centralized_critic", "--total-timesteps=10000000"],
 ]
+
+
 
 def run_experiment(experiment):
     print("Running experiment: ", experiment)
