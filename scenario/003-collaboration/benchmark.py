dcccf4527e627278c931b535105107b5b029b519
thlamz <thlamenza@hotmail.com>
2024-07-14 20:23:19 -0300

    Setting up benchmark with optimal hyperparameters

--- scenario/003-collaboration/benchmark.py
+++ scenario/003-collaboration/benchmark.py
@@ -13,13 +13,17 @@ class Args:
 # python main.py --num-drones=2 --num-sensors=2 --run-name=centralized-critic --exp-name=yes --state_num_closest_sensors=2 --state-num-closest-drones=1 --min-sensor-priority=1 --centralized_critic --total-timesteps=10000000
 
 # Write the commands above in array form
+# experiments = [
+#     ["python", "main.py", "--num-drones=2", "--num-sensors=12", "--run-name=solution1", f"--exp-name=2-heuristics", "--state_num_closest_sensors=12", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--total-timesteps=500000", "--checkpoint-freq=100000", "--use-heuristics"],
+#     ["python", "main.py", "--num-drones=2", "--num-sensors=12", "--run-name=solution1", f"--exp-name=2-learning", "--state_num_closest_sensors=12", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--total-timesteps=50000000", "--checkpoint-freq=100000", ],
+#     ["python", "main.py", "--num-drones=4", "--num-sensors=12", "--run-name=solution1", f"--exp-name=4-heuristics", "--state_num_closest_sensors=12", "--state-num-closest-drones=3", "--min-sensor-priority=1", "--total-timesteps=500000", "--checkpoint-freq=100000", "--use-heuristics"],
+#     ["python", "main.py", "--num-drones=4", "--num-sensors=12", "--run-name=solution1", f"--exp-name=4-learning", "--state_num_closest_sensors=12", "--state-num-closest-drones=3", "--min-sensor-priority=1", "--total-timesteps=50000000", "--checkpoint-freq=100000", ],
+#     ["python", "main.py", "--num-drones=6", "--num-sensors=12", "--run-name=solution1", f"--exp-name=6-heuristics", "--state_num_closest_sensors=12", "--state-num-closest-drones=5", "--min-sensor-priority=1", "--total-timesteps=500000", "--checkpoint-freq=100000", "--use-heuristics"],
+#     ["python", "main.py", "--num-drones=6", "--num-sensors=12", "--run-name=solution1", f"--exp-name=6-learning", "--state_num_closest_sensors=10", "--state-num-closest-drones=5", "--min-sensor-priority=1", "--total-timesteps=50000000", "--checkpoint-freq=100000", ],
+# ]
+
 experiments = [
-    ["python", "main.py", "--num-drones=2", "--num-sensors=12", "--run-name=solution", f"--exp-name=2-heuristics", "--state_num_closest_sensors=12", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--total-timesteps=50000000", "--checkpoint-freq=100000", "--use-heuristics"],
-    ["python", "main.py", "--num-drones=2", "--num-sensors=12", "--run-name=solution", f"--exp-name=2-learning", "--state_num_closest_sensors=12", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--total-timesteps=50000000", "--checkpoint-freq=100000", ],
-    ["python", "main.py", "--num-drones=4", "--num-sensors=12", "--run-name=solution", f"--exp-name=4-heuristics", "--state_num_closest_sensors=12", "--state-num-closest-drones=3", "--min-sensor-priority=1", "--total-timesteps=50000000", "--checkpoint-freq=100000", "--use-heuristics"],
-    ["python", "main.py", "--num-drones=4", "--num-sensors=12", "--run-name=solution", f"--exp-name=4-learning", "--state_num_closest_sensors=12", "--state-num-closest-drones=3", "--min-sensor-priority=1", "--total-timesteps=50000000", "--checkpoint-freq=100000", ],
-    ["python", "main.py", "--num-drones=6", "--num-sensors=12", "--run-name=solution", f"--exp-name=6-heuristics", "--state_num_closest_sensors=12", "--state-num-closest-drones=5", "--min-sensor-priority=1", "--total-timesteps=50000000", "--checkpoint-freq=100000", "--use-heuristics"],
-    ["python", "main.py", "--num-drones=6", "--num-sensors=12", "--run-name=solution", f"--exp-name=6-learning", "--state_num_closest_sensors=12", "--state-num-closest-drones=5", "--min-sensor-priority=1", "--total-timesteps=50000000", "--checkpoint-freq=100000", ],
+    ["python", "main.py", "--num-drones=6", "--num-sensors=12", "--run-name=optimized", f"--exp-name=6-learning", "--state_num_closest_sensors=8", "--state-num-closest-drones=5", "--min-sensor-priority=1", "--total-timesteps=50000000", "--checkpoint-freq=100000", "--punish-reward"],
 ]
 
 print("Total experiments: ", len(experiments))
