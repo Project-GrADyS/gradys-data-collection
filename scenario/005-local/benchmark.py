e064e2f0293562fc09cfa9ed2fea070c93562e83
Thlamz <thlamenza@hotmail.com>
2025-02-02 14:10:01 -0300

    Setting up local info benchmark

--- scenario/005-local/benchmark.py
+++ scenario/005-local/benchmark.py
@@ -9,32 +9,23 @@ import tyro
 class Args:
     concurrency: int = 3
 
-# Commands:
-# python main.py --num-drones=2 --num-sensors=2 --run-name=centralized-critic --exp-name=yes --state_num_closest_sensors=2 --state-num-closest-drones=1 --min-sensor-priority=1 --centralized_critic --total-timesteps=10000000
-
-# experiments = [
-#     ["python", "main.py", "--num-drones=2", "--num-sensors=12", "--run-name=solution1", f"--exp-name=2-random", "--state_num_closest_sensors=12", "--state-num-closest-drones=1", "--min-sensor-priority=1", "--total-timesteps=500000", "--checkpoint-freq=100000", "--use-heuristics=random"],
-#     ["python", "main.py", "--num-drones=4", "--num-sensors=12", "--run-name=solution1", f"--exp-name=4-random", "--state_num_closest_sensors=12", "--state-num-closest-drones=3", "--min-sensor-priority=1", "--total-timesteps=500000", "--checkpoint-freq=100000", "--use-heuristics=random"],
-#     ["python", "main.py", "--num-drones=6", "--num-sensors=12", "--run-name=solution1", f"--exp-name=6-random", "--state_num_closest_sensors=12", "--state-num-closest-drones=5", "--min-sensor-priority=1", "--total-timesteps=500000", "--checkpoint-freq=100000", "--use-heuristics=random"],
-# ]
-
 experiments = [
 ]
 
-for agents in [8]:
+for agents in [4]:
     for sensors in [(12, 36)]:
-        for local_observation in [False]:
-            for global_state in [False]:
-                experiments.append(["python", "main.py", f"--num-drones={agents}", f"--min-num-sensors={sensors[0]}",
-                    "--exp-name=varying_sensor_results", f"--max-num-sensors={sensors[1]}",
-                    f"--run-name=a_8-s_(12,36)", "--min-sensor-priority=1", "--total-timesteps=50000000", 
-                    "--checkpoint-freq=1000000", "--algorithm-iteration-interval=0.5",
-                    "--actor-learning-rate=0.000001", "--critic-learning-rate=0.000001", 
-                    f"--state-num-closest-drones={agents-1}", f"--state-num-closest-sensors=12", 
-                    "--reward=punish", "--no-end-when-all-collected", 
-                    "--local-observation" if local_observation else "--no-local-observation",
-                    "--critic-global-state" if global_state else "--no-critic-global-state"
-                    ])
+        for local_obs in [True]:
+            experiments.append(["python", "main.py", f"--num-drones={agents}", f"--min-num-sensors={sensors[0]}",
+                "--exp-name=time_on_state", f"--max-num-sensors={sensors[1]}",
+                f"--run-name={agents}a {sensors}s ", "--min-sensor-priority=1",
+                "--total-timesteps=100000000",
+                "--checkpoint-freq=1000000", "--algorithm-iteration-interval=2",
+                "--actor-learning-rate=0.00001", "--critic-learning-rate=0.00001",
+                f"--state-num-closest-drones={agents-1}", f"--state-num-closest-sensors=12",
+                "--reward=punish", "--no-end-when-all-collected", "--no-speed-action",
+                "--local-observation" if local_obs else "--no-local-observation",
+                "--critic-global-state"
+                ])
 
 
 print("Total experiments: ", len(experiments))
