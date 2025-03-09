05c1f9c94dc22300dacbbf675774a464d1f28d07
thlamz <thlamenza@hotmail.com>
2025-03-09 10:17:17 -0300

    Good benchmark config

--- scenario/005-local/benchmark.py
+++ scenario/005-local/benchmark.py
@@ -14,17 +14,30 @@ experiments = [
 
 for agents in [4]:
     for sensors in [(12, 36)]:
-        for local_obs in [True]:
-            experiments.append(["python", "main.py", f"--num-drones={agents}", f"--min-num-sensors={sensors[0]}",
-                "--exp-name=time_on_state", f"--max-num-sensors={sensors[1]}",
-                f"--run-name={agents}a {sensors}s ", "--min-sensor-priority=1",
+        for interval in [0.5]:
+            experiments.append(["python3", "main.py", 
+                f"--num-drones={agents}", 
+                f"--min-num-sensors={sensors[0]}",
+                f"--max-num-sensors={sensors[1]}",
+                "--exp-name=local-benchmark", 
+                f"--run-name={agents}a {sensors}s {interval} interval more memory", 
+                "--buffer-size=10000000",
+                "--min-sensor-priority=1",
                 "--total-timesteps=100000000",
-                "--checkpoint-freq=1000000", "--algorithm-iteration-interval=2",
-                "--actor-learning-rate=0.00001", "--critic-learning-rate=0.00001",
-                f"--state-num-closest-drones={agents-1}", f"--state-num-closest-sensors=12",
-                "--reward=punish", "--no-end-when-all-collected", "--no-speed-action",
-                "--local-observation" if local_obs else "--no-local-observation",
-                "--critic-global-state"
+                "--checkpoint-freq=1000000",
+                f"--algorithm-iteration-interval={interval}",
+                "--actor-learning-rate=0.0001", 
+                "--critic-learning-rate=0.0001",
+                f"--state-num-closest-drones={agents-1}", 
+                "--state-num-closest-sensors=12",
+                "--reward=punish", 
+                "--no-end-when-all-collected", 
+                "--speed-action",
+                "--local-observation",
+                "--batch-size=512",
+                "--critic-global-state",
+                "--max-episode-length=800",
+                "--max-seconds-stalled=80"
                 ])
 
 
